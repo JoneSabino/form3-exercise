29459e2f19f84e23fe39113f28094d27a5caac79
Jones Sabino <jones.sabino@gmail.com>
2021-08-21 12:11:11 -0300

    function development

--- form3/accounts.go
+++ form3/accounts.go
@@ -3,48 +3,70 @@ package accounts
 import (
 	"bytes"
 	"encoding/json"
+	"io/ioutil"
+	"strings"
+
 	// "io/ioutil"
 	// "log"
 	"net/http"
 )
 
 const (
-	URL = "http://localhost:8080/v1/organisation/accounts"
+	uRL = "http://localhost:8080/v1/organisation/accounts/"
 )
 
 // POST /v1/organisation/accounts
-func Create(data map[string]string) (*http.Response, error) {
-	// payload := model.AccountData{}
-    // json.Unmarshal([]byte(data), &payload)
-	body, _ := json.Marshal(data)
-	// reqBody := bytes.NewReader(body)
-
-	resp, err := http.Post(URL,"application/json",bytes.NewBuffer(body))
-	// respBody, err := ioutil.ReadAll(resp.Body)
-	// respCode := string(resp.StatusCode)
-
-	return resp, err
+func Create(payload string) (*http.Response, error) {
+	reqBody := strings.NewReader(payload)
+	
+	res, err := http.Post(uRL, "application/json", reqBody)
+	if err != nil {
+		return nil, err
+	}
+	// body, _ := ioutil.ReadAll(res.Body)
+	// res.Body.Close()
+	return res, nil
 }
 
-// // GET /v1/organisation/accounts/{account_id}
-// func Fetch(fiat string, crypto string) (string, error) {
-// 	//Build The URL string
+// GET /v1/organisation/accounts/{account_id}
+func Fetch(accountId string) (*http.Response, error) {
+	//Build The URL string
 
-// 	//We make HTTP request using the Get function
-// 	resp, err := http.Get(URL)
-// 	if err != nil {
-// 		log.Fatal("ooopsss an error occurred, please try again")
-// 	}
-// 	defer resp.Body.Close()
-// 	//Create a variable of the same type as our model
-// 	var cResp model.Cryptoresponse
-// 	//Decode the data
-// 	if err := json.NewDecoder(resp.Body).Decode(&cResp); err != nil {
-// 		log.Fatal("ooopsss! an error occurred, please try again")
-// 	}
-// 	//Invoke the text output function & return it with nil as the error value
-// 	return cResp.TextOutput(), nil
-// }
+	//We make HTTP request using the Get function
+	res, err := http.Get(uRL + accountId)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	//Create a variable of the same type as our model
+	
+	//Invoke the text output function & return it with nil as the error value
+	return res, nil
+}
 
-// // DELETE /v1/organisation/accounts/{account_id}?version={version}
-// func Delete() {}
+// DELETE /v1/organisation/accounts/{account_id}?version={version}
+func Delete(accountId string, version string) (*http.Response, error){
+	// Create client
+    client := &http.Client{}
+
+    // Create request
+    req, err := http.NewRequest("DELETE", uRL + accountId, nil)
+    if err != nil {
+        return nil, err
+    }
+
+    // Fetch Request
+    resp, err := client.Do(req)
+    if err != nil {
+        return nil, err
+    }
+    defer resp.Body.Close()
+
+    // Read Response Body
+    // respBody, err := ioutil.ReadAll(resp.Body)
+    // if err != nil {
+    //     return nil, err
+    // }
+
+    return resp, nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/JoneSabino/form3-exercise
 
 go 1.16
+
+require github.com/satori/go.uuid v1.2.0
