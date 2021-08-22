79c4abc0d2bc97e967d47d383e9da0c88044cb2e
Jones Sabino <jones.sabino@gmail.com>
2021-08-22 20:42:53 -0300

    error handling

--- form3/accounts.go
+++ form3/accounts.go
@@ -3,13 +3,13 @@ package accounts
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
+	"errors"
+	"github.com/JoneSabino/form3-exercise/model"
+	uuid "github.com/satori/go.uuid"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"time"
-
-	"github.com/JoneSabino/form3-exercise/model"
+	"strconv"
 )
 
 const (
@@ -23,37 +23,57 @@ const (
 //  - `AccAttr`: model.AccountAttribute
 // Request: POST /v1/organisation/accounts
 func Create(accData model.AccountData) (model.Account, error) {
-	// id := uuid.NewV4().String()
-	id := "ad27e265-9605-4b4b-a0e5-3003ea9cc4dc"
+	id := uuid.NewV4().String()
+	// id := "ad27e265-9605-4b4b-a0e5-3003ea9cc4dc"
 	accData.Type = "accounts"
 	accData.ID = id
 
 	payload := model.Account{
 		Data: &accData,
 	}
-
+	
 	payloadJson, err := json.Marshal(payload)
-	reqBody := bytes.NewBuffer(payloadJson)
-
 	if err != nil {
-		return model.Account{}, err
+		return model.Account{}, errors.New(err.Error() +
+		"\nFailed to marshal the payload")
 	}
+
 	var resp *http.Response
+	var respBody []byte
 	for i := 0; i < 3; i++ {
-		resp, err := http.Post(uRL, "application/json", reqBody)
+		reqBody := bytes.NewBuffer(payloadJson)
+		resp, err = http.Post(uRL, "application/json", reqBody)
 		if err != nil {
-			return model.Account{}, err
+			return model.Account{}, errors.New(err.Error() +
+				"\nFailed sending the POST request")
 		}
 
-		if resp.StatusCode == 409 {
+		if resp.StatusCode == http.StatusConflict {
 			log.Println(resp.Status + ". Probably the value of the `id` field alredy exists. Retrying...")
-			time.Sleep(1)
+			if i == 2 {
+				return model.Account{}, errors.New(strconv.Itoa(resp.StatusCode) + ": Duplicated Account ID")
+			}
+			continue
+		} else if resp.StatusCode != http.StatusCreated {
+			respBody, err = ioutil.ReadAll(resp.Body)
+			if err != nil {
+				return model.Account{}, errors.New(err.Error() +
+					"\nThere was a problem when trying to read the response body")
+			}
+			return model.Account{}, errors.New(resp.Status + "\n" +
+				string(respBody))
+		} else {
+			break
 		}
 	}
 
-	respBody, err := ioutil.ReadAll(resp.Body)
-	var accModel model.Account
-	err = json.Unmarshal(respBody, &accModel)
+	respBody, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return model.Account{}, errors.New(err.Error() +
+			"\nThere was a problem when trying to read the response body")
+	}
+
+	accModel, err := mapResponse(respBody)
 
 	return accModel, nil
 }
@@ -66,49 +86,63 @@ func Create(accData model.AccountData) (model.Account, error) {
 func Fetch(accountId string) (model.Account, error) {
 
 	resp, err := http.Get(uRL + accountId)
+	if err != nil {
+		return model.Account{}, errors.New(err.Error() +
+			"\nFailed sending the GET request")
+	}
 
 	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return model.Account{}, errors.New(err.Error() +
+			"\nThere was a problem when trying to read the response body")
+	}
 
 	if resp.StatusCode != http.StatusOK {
-		fmt.Println("ERROR - Status Code:", resp.StatusCode)
-		return model.Account{}, err
+		log.Println("ERROR - Status Code:", resp.StatusCode)
+		return model.Account{}, errors.New(string(respBody))
 	}
 
-	if err != nil {
-		panic(err)
-	}
-
-	var respModel model.Account
-	err = json.Unmarshal(respBody, &respModel)
-
-	if err != nil {
-		panic(err)
-	}
+	respModel, err := mapResponse(respBody)
 
 	return respModel, nil
 }
 
 // Deletes the account specified.
 //
-// Returns a string with the response status:
-//  - Success: '204 No Content'
+// Returns a string with the response status.
+//
+// A successul request will return '204 No Content'
 //Parameters:
-//	- `AccountId` :
+//	- `AccountId` : string
+//	- `Version` : string
 // Request: DELETE /v1/organisation/accounts/{account_id}?version={version}
 func Delete(accountId string, version string) (string, error) {
 	client := &http.Client{}
 
 	req, err := http.NewRequest("DELETE", uRL+accountId, nil)
 	if err != nil {
-		return "", err
+		return "", errors.New(err.Error() +
+			"\n Error while creating the request")
 	}
+
 	q := req.URL.Query()
 	q.Add("version", version)
 	req.URL.RawQuery = q.Encode()
 
 	resp, err := client.Do(req)
 	if err != nil {
-		return "", err
+		return "", errors.New(err.Error() +
+			"\n Failed sending the DELETE request")
 	}
 	return resp.Status, nil
+}
+
+func mapResponse(respBody []byte) (model.Account, error) {
+	var accModel model.Account
+	err := json.Unmarshal(respBody, &accModel)
+	if err != nil {
+		return model.Account{}, errors.New(err.Error() +
+			"\nFailed when trying map the response body to the Account structure")
+	}
+	return accModel, nil
 }
